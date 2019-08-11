531ef9fcf54b6f79dd5a5d7b9a46095a0a1be740
Chandan09Soni <chandan.09soni@gmail.com>
2019-08-12 00:50:47 +0530

    Adding machine learning library

--- CSDevelopment/hello_test.py
+++ CSDevelopment/hello_test.py
@@ -1,23 +1,32 @@
-#!/usr/bin/python3
+
 import sys
-import boto3
+# from pyspark.sql import SparkSession
+# import boto3
+from pyspark.sql import SparkSession
+
+
 class HelloTest:
-    """asdkfjl"""
+    """This is a test class"""
     def __init__(self):
         """This is the intialization"""
         print('This is initializatoin function')
+        self.app_name = self.__class__.__name__
+        self.spark_session = SparkSession.builder.appName(self.app_name).\
+            getOrCreate()
 
     def loader(self):
         """This is a main entry point of class"""
+        print(self.app_name)
         print(' loader function ')
-        print(sys.version)
+        print('Python version is {}'.format(sys.version))
         words = ['chandu', 'kumar', 'soni', 'chandan']
         for word in words[:]:
             if len(word) > 6:
                 words.insert(0, word)
         print('Index \t Content')
         for index, word in enumerate(words):
-            print(str(index) +'\t'+ word)
+            print(str(index) + '\t' + word)
+
 
 if __name__ == '__main__':
     HelloTest().loader()
